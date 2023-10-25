818c3908de758b2eba4b0bff2e72fcfc3413e064
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-10-25 09:42:05 -0400

    init

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ app.use(express.static(__dirname + "/static"), (_, res, next) => {
   res.sendFile(__dirname + "/403.html")
   res.status(401)
   res.sendFile(__dirname + "/401.html")
-  res.status(406)
-  res.sendFile(__dirname + "/406.html")
-  res.status(500)
-  res.sendFile(__dirname + "/500.html")
-  res.status(301)
-  res.sendFile(__dirname + "/301.html")
 });
 
 app.listen(8080);
